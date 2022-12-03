f1ce04fe5fb094019a2cdf7788ae50d9cb744505
Meghsham-Jambhulkar <106965064+Meghsham-Jambhulkar@users.noreply.github.com>
2022-12-03 21:16:27 +0530

    Delete Assignment2 directory

--- Assignment2/Assignment2_9.py
+++
@@ -1,8 +0,0 @@
-#Write a program which accepts numbers from user and returns numbers of digits in that number 
-def main():
-    print("Enter a number :")
-    no = input()
-    print("The Length of the input number is  : ",len(no))
-
-if __name__ == "__main__":
-    main()
